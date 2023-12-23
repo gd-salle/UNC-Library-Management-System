2997cbc7df704b450d77d5f085e4f3ef7f8ac7ac
BenzClerigo <benedick.clerigo@unc.edu.ph>
2023-12-23 12:56:55 +0800

    Main file. Download or clone this. This will be our main project.

---
+++ src/main/java/org/unc/lms/codes/LibraryManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package org.unc.lms.codes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//Bootstrap Class
+@SpringBootApplication
+public class LibraryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementSystemApplication.class, args); 
+	}
+}
---
+++ src/main/java/org/unc/lms/codes/SpringMVContainerConfig.java
@@ -0,0 +1,67 @@
+package org.unc.lms.codes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "org.unc.lms.codes")
+public class SpringMVContainerConfig implements WebMvcConfigurer{
+	
+	@Autowired
+	public ApplicationContext applicationContext; // non-MVC
+    
+	@Bean
+	public SpringResourceTemplateResolver templateViewResolver() {
+		SpringResourceTemplateResolver templateViewResolver = 
+				new SpringResourceTemplateResolver();
+		templateViewResolver.setApplicationContext(applicationContext);
+		templateViewResolver.setPrefix("/WEB-INF/views/");
+		templateViewResolver.setSuffix(".html");
+		templateViewResolver.setTemplateMode(TemplateMode.HTML);
+		templateViewResolver.setCacheable(true);
+		return templateViewResolver;
+	}
+	
+	@Bean
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine templateEngine = new  SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateViewResolver());
+		templateEngine.setEnableSpringELCompiler(true);
+		return templateEngine;
+	}
+	
+	@Bean
+	public ThymeleafViewResolver thymeleafResolver() {
+		ThymeleafViewResolver thymeleafResolver = new ThymeleafViewResolver();
+		thymeleafResolver.setTemplateEngine(templateEngine());
+		return thymeleafResolver;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		
+		registry.addResourceHandler(
+									"/css/**",
+									"/files/**",
+									"/img/**", 
+									"/js/**")
+		.addResourceLocations(
+							  "classpath:/static/css/",
+							  "classpath:/static/files/",
+							  "classpath:/static/img/",
+							  "classpath:/static/js/"); 
+		        
+	}
+	
+}
