3ce76642554ea17a5540723601d3b55975bef455
gd-salle <gerardojr.salle@gmail.com>
2024-01-03 13:40:54 +0800

    added book data

---
+++ src/main/java/org/unc/lms/codes/model/data/Book.java
@@ -0,0 +1,50 @@
+package org.unc.lms.codes.model.data;
+
+import java.util.Date;
+
+public class Book {
+
+	private int id;
+	private String title;
+	private String author;
+	private String genre;
+	private Date yearPublished;
+	private Student bookBorrower;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public Date getYearPublished() {
+		return yearPublished;
+	}
+	public void setYearPublished(Date yearPublished) {
+		this.yearPublished = yearPublished;
+	}
+	public Student getBookBorrower() {
+		return bookBorrower;
+	}
+	public void setBookBorrower(Student bookBorrower) {
+		this.bookBorrower = bookBorrower;
+	}
+}
