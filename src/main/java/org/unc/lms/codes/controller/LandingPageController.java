259ea997211050a1715effeb3bafdbde4431d897
BenzClerigo <benedick.clerigo@unc.edu.ph>
2024-01-09 11:32:33 +0800

    Final?

--- src/main/java/org/unc/lms/codes/controller/LandingPageController.java
+++ src/main/java/org/unc/lms/codes/controller/LandingPageController.java
@@ -2,18 +2,10 @@ package org.unc.lms.codes.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.unc.lms.codes.model.form.LibraryRegistrationForm;
@@ -52,5 +44,4 @@ public class LandingPageController {
 	    return "redirect:/login?loginRequest";
 	}
 
-	
 }
